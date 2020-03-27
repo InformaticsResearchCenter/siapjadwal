7030a771b3038c27984430ac1024b0f6737efe04
Diva Krishna <divakrishnam@gmail.com>
2020-03-27 20:54:06 +0700

    remove user and pass, add xls file

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import pandas as pd
 import string
 from selenium.webdriver import ChromeOptions, Chrome
 
-username = 'siap_akademik'
-password = 'poltek'
+username = 'xxxx'
+password = 'xxxx'
 tahun = '20191'
 jenis = '1'
 program = 'REG'
@@ -16,12 +16,14 @@ opts = ChromeOptions()
 opts.add_experimental_option("detach", True)
 browser = Chrome(options=opts)
 
-#define browser
+# define browser
+
+
 def launchBrowser():
-    #open siap
+    # open siap
     browser.get("http://siap.poltekpos.ac.id/")
 
-    #login to siap
+    # login to siap
     user_siap = browser.find_element_by_xpath("//input[@name='user_name']")
     user_siap.send_keys(username)
     pass_siap = browser.find_element_by_xpath("//input[@name='user_pass']")
@@ -29,9 +31,10 @@ def launchBrowser():
     login_siap = browser.find_element_by_xpath("//input[@name='login']")
     login_siap.send_keys(Keys.ENTER)
 
-    #choose jadwal menu
+    # choose jadwal menu
     jadwal_menu = browser.find_element_by_link_text("Jadwal Ujian 1")
     jadwal_menu.click()
+
 
 def convertKelas(kelas):
     kelas = kelas[-1:].lower()
@@ -42,6 +45,7 @@ def convertKelas(kelas):
         if k == kelas:
             return v
             break
+
 
 def setJadwal(tabel_select, matkul_selected, kelas_selected):
     index = 1
@@ -65,19 +69,24 @@ def setJadwal(tabel_select, matkul_selected, kelas_selected):
             return True
             break
 
+
 def setUjian(prodi_selected, matkul_selected, kelas_selected):
     prodi_selected = prodi_selected.lower()
     matkul_selected = matkul_selected.lower()
     kelas_selected = convertKelas(kelas_selected)
 
-    #choose tahun
-    tahun_ujian = Select(browser.find_element_by_xpath("//select[@name='tahun']"))
+    # choose tahun
+    tahun_ujian = Select(
+        browser.find_element_by_xpath("//select[@name='tahun']"))
     tahun_ujian.select_by_value(tahun)
-    jenis_ujian = Select(browser.find_element_by_xpath("//select[@name='ujian']"))
+    jenis_ujian = Select(
+        browser.find_element_by_xpath("//select[@name='ujian']"))
     jenis_ujian.select_by_value(jenis)
-    program_ujian = Select(browser.find_element_by_xpath("//select[@name='prid']"))
+    program_ujian = Select(
+        browser.find_element_by_xpath("//select[@name='prid']"))
     program_ujian.select_by_value(program)
-    prodi_ujian = Select(browser.find_element_by_xpath("//select[@name='prodi']"))
+    prodi_ujian = Select(
+        browser.find_element_by_xpath("//select[@name='prodi']"))
     for prodis in prodi_ujian.options:
         prodi = prodis.text[5:].lower()
         if prodi_selected in prodi:
@@ -88,27 +97,34 @@ def setUjian(prodi_selected, matkul_selected, kelas_selected):
     tampil_ujian.send_keys(Keys.ENTER)
 
     time.sleep(2)
-    tabel_select = browser.find_element_by_xpath("//table[@cellpadding='4' and @cellspacing='1']/tbody")
+    tabel_select = browser.find_element_by_xpath(
+        "//table[@cellpadding='4' and @cellspacing='1']/tbody")
 
     if setJadwal(tabel_select, matkul_selected, kelas_selected):
-        tanggal_ujian = Select(browser.find_element_by_xpath("//select[@name='TGL_d']"))
+        tanggal_ujian = Select(
+            browser.find_element_by_xpath("//select[@name='TGL_d']"))
         tanggal_ujian.select_by_visible_text('25')
-        bulan_ujian = Select(browser.find_element_by_xpath("//select[@name='TGL_m']"))
+        bulan_ujian = Select(
+            browser.find_element_by_xpath("//select[@name='TGL_m']"))
         bulan_ujian.select_by_value('11')
-        tahun_ujian = Select(browser.find_element_by_xpath("//select[@name='TGL_y']"))
+        tahun_ujian = Select(
+            browser.find_element_by_xpath("//select[@name='TGL_y']"))
         tahun_ujian.select_by_visible_text('2019')
         jam_mulai_ujian = browser.find_element_by_xpath("//input[@name='JM']")
         jam_mulai_ujian.send_keys('08:00:00')
-        jam_selesai_ujian = browser.find_element_by_xpath("//input[@name='JS']")
+        jam_selesai_ujian = browser.find_element_by_xpath(
+            "//input[@name='JS']")
         jam_selesai_ujian.send_keys('10:00:00')
-        ruangan_ujian = browser.find_element_by_xpath("//input[@name='RuangID1']")
+        ruangan_ujian = browser.find_element_by_xpath(
+            "//input[@name='RuangID1']")
         ruangan_ujian.send_keys('')
         return True
 
 
 ###### Mulai disini ######
 
-data = pd.read_excel(r'revisi JADWAL UTS GANJIL 2019 2020 - 1.xls',skiprows=3, usecols=['MATAKULIAH', 'KELAS', 'PRODI'])
+data = pd.read_excel(r'revisi JADWAL UTS GANJIL 2019 2020 - 1.xls',
+                     skiprows=3, usecols=['MATAKULIAH', 'KELAS', 'PRODI'])
 data = data.dropna()
 
 launchBrowser()
@@ -120,5 +136,4 @@ launchBrowser()
 #     print(matkul, kelas, prodi)
 
 setUjian('D4 Teknik Informatika', 'Matematika Diskrit', '2A')
-
 
